cd3de02caf89bfda26219c8a994e21aee4f4d00c
jhpark-coder <pjh133765@gmail.com>
2025-05-14 08:34:10 +0900

    Add files via upload

---
+++ MineSweeper/src/TimeChecker.java
@@ -0,0 +1,10 @@
+public class TimeChecker {
+
+    public long TimeCheck(){
+        Thread timerThread = new Thread(() -> {
+            long startNanoTimes = System.nanoTime();
+
+
+        }
+    }
+}
